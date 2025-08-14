344e182ef74e01dafc701b77af0564afd90849d7
Taha Fatih Kaya <tahafatihkaya@Taha-MacBook-Air.local>
2025-08-14 10:23:49 +0300

    If-Else added

--- 03-If-Else/If-Else/If-Else/main.swift
+++ 03-If-Else/If-Else/If-Else/main.swift
@@ -14,3 +14,22 @@ else if sum > 10 {
 else {
     print("sum<10")
 }
+
+let name: String? = "Taha"
+let age: Int? = 24
+let isStudent = true
+let gender = "male"
+let currentYear = Calendar.current.component(.year, from: Date())
+var bornYear: Int? = nil
+if let age = age {
+    bornYear = currentYear - age
+}
+else {
+    bornYear = nil
+}
+if (name != nil && age != nil && bornYear != nil ) && (isStudent && gender == "male")   {
+    print("Hello \(name!), your are \(age!) years old, You were born in \(bornYear!)")
+}
+else {
+    print("No INFO or missing parameters")
+}
